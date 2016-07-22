b234f8bab2d5b3e0983c4ce5acdab422c435d182
rayNymous <nymous@bakas.it>
2016-07-22 20:17:47 +0300

    simple unity/erlang client for echo server

---
+++ Assets/Scripts/Message.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Message {
+
+    public static int POCKETSIZELEN=2; //for 2 bytes ushort length type
+ //   public static int POCKETSIZELEN = 4; // for uint32 4 bytes type 
+
+    public ushort Length { get; set; }
+    public byte[] Content { get; set; }
+
+    public Message(byte[] data)
+    {
+        Content = data;
+        Length = (ushort)data.Length;
+    }
+
+    public static Message ReadFromStream(BinaryReader reader)
+    {
+        ushort len;
+        byte[] len_buf;
+        byte[] buffer;
+
+        len_buf = reader.ReadBytes(POCKETSIZELEN);
+        if (BitConverter.IsLittleEndian)
+        {
+            Array.Reverse(len_buf);
+        }
+        len = BitConverter.ToUInt16(len_buf, 0);
+        buffer = reader.ReadBytes(len);
+
+        return new Message(buffer);
+    }
+
+    public void WriteToStream(BinaryWriter writer)
+    {
+        byte[] len_bytes = BitConverter.GetBytes(Length);
+
+        if (BitConverter.IsLittleEndian)
+        {
+            Array.Reverse(len_bytes);
+        }
+        writer.Write(len_bytes);
+        writer.Write(Content);
+    }
+
+    public static byte[] GetBytes(string str)
+    {
+        byte[] bytes = new byte[str.Length * sizeof(char)];
+        System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+        return bytes;
+    }
+
+    public static string GetString(byte[] bytes)
+    {
+        char[] chars = new char[bytes.Length / sizeof(char)];
+        System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+        return new string(chars);
+    }
+}
---
+++ Assets/Scripts/NetworkController.cs
@@ -0,0 +1,124 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+
+using UnityEngine.UI;
+
+public class NetworkController : MonoBehaviour
+{
+    public static string serverName = "127.0.0.1";
+    public static int serverPort = 3333;
+
+    static TcpClient _client;
+    static BinaryReader _reader;
+    static BinaryWriter _writer;
+    static Thread networkThread;
+
+    private static Queue<Message> _messageQueue = new Queue<Message>();
+
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartServer();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    ProcessMessage();
+	}
+
+    static void AddItemToQueue(Message item)
+    {
+        lock (_messageQueue)
+        {
+            _messageQueue.Enqueue(item);
+        }
+    }
+
+    static Message GetItemFromQueue()
+    {
+        lock (_messageQueue)
+        {
+            if (_messageQueue.Count > 0)
+            {
+                return _messageQueue.Dequeue();
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+
+    static void ProcessMessage()
+    {
+        Message msg = GetItemFromQueue();
+        if (msg != null)
+        {
+            Debug.LogWarning("message received: " + Message.GetString(msg.Content).ToString());
+            //process current message is here
+        }
+    }
+
+    public static void SendMessage(Message msg)
+    {
+        msg.WriteToStream(_writer);
+        _writer.Flush();
+    }
+
+    static void StartServer()
+    {
+        if (networkThread == null)
+        {
+            Connect();
+            networkThread = new Thread(() => {
+                                                 while (_reader != null)
+                                                 {
+                                                     Message msg = Message.ReadFromStream(_reader);
+                                                     AddItemToQueue(msg);
+                                                 }
+                                                 lock (networkThread)
+                                                 {
+                                                     networkThread = null;
+                                                 }
+            });
+            networkThread.Start();
+        }
+    }
+
+    static void Connect()
+    {
+        if (_client == null)
+        {
+            _client = new TcpClient(serverName, serverPort);
+            Stream stream = _client.GetStream();
+            _reader = new BinaryReader(stream);
+            _writer = new BinaryWriter(stream);
+
+            Debug.Log("Connected <3");
+        }
+    }
+
+    public void SendMessageString()
+    {
+        GameObject go = GameObject.FindGameObjectWithTag("INPUT");
+        string s = (go.transform.GetComponent<Text>()).text;
+
+
+        Debug.Log("trying to send message with text: "+ s);
+        byte[] bytes = Message.GetBytes(s);
+        Message m = new Message(bytes);
+        SendMessage(m);
+        Debug.Log("message: " + s + " send. love chu <3");
+    }
+}
